6f5fb7e86188b415da7fc5551809105d90147806
条件状语从句 <lixzap@gmail.com>
2018-05-29 16:34:12 +0800

    first

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+const router = require('koa-router')()
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+
+router.post('/name/:name', async (ctx, next) => {
+  ctx.body = `I am ${ctx.params.name}!`;
+})
+
+router.get('/json', async (ctx, next) => {
+  ctx.body = {
+    title: 'koa2 json'
+  }
+})
+
+module.exports = router
